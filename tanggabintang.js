818c0a1ee1305247e5686c24253d60d3276b80bd
rafi <rafiharlian@gmail.com>
2020-04-20 12:58:37 +0700

    update dengan kondisi

--- tanggabintang.js
+++ tanggabintang.js
@@ -1,8 +1,21 @@
+// let rows2 = 5
+// for (let i = 1; i <= rows2; i++){
+//     let bintang = ''
+//     for(let j = 1; j <= i; j++){
+//       bintang += '*'
+//     }
+//     console.log(bintang)
+// }
+
+
+// dengan kondisi if
 let rows2 = 5
 for (let i = 1; i <= rows2; i++){
-    let j = ''
-    for(let k = 1; k <= i; k++){
-        j += '*'
+    let bintang = ''
+    for(let j = 1; j <= i; j++){
+    if(i >= j){
+        bintang += '*'       
+        }
     }
-    console.log(j)
+    console.log(bintang)
 }
